9ea126b9baf82a07d8b77361cd7771c1b73045fd
Fabien Aubret <fabien.aubret@gmail.com>
2019-11-30 17:53:58 +0100

    [Add] Add RESTFul API for shops

--- borgia/rest/urls.py
+++ borgia/rest/urls.py
@@ -7,7 +7,9 @@ rest_patterns = [
         path('users/', views.UserList.as_view()),
         path('users/<int:pk>/', views.UserDetail.as_view()),
         path('products/', views.ProductList.as_view()),
-        path('products/<int:pk>/', views.ProductDetail.as_view())
+        path('products/<int:pk>/', views.ProductDetail.as_view()),
+        path('shops/', views.ShopList.as_view()),
+        path('shops/<int:pk>/', views.ShopDetail.as_view())
     ])),
     path('api-auth/', include('rest_framework.urls', namespace='rest-framework'))
 ]
