b6e505e1294e891c853f99d40409df3e6df4c2b8
Fabien Aubret <fabien.aubret@gmail.com>
2019-11-30 15:42:47 +0100

    [Update] Implements user listing and detailed display

--- borgia/rest/urls.py
+++ borgia/rest/urls.py
@@ -1,9 +1,13 @@
 from django.urls import include, path
+from rest_framework.urlpatterns import format_suffix_patterns
 from rest import views
 
 rest_patterns = [
     path('api/', include([
-        path('users', views.UserViewSet.as_view({'get': 'list'}), name='rest_user_list')
+        path('users/', views.UserList.as_view()),
+        path('users/<int:pk>/', views.UserDetail.as_view())
     ])),
     path('api-auth/', include('rest_framework.urls', namespace='rest-framework'))
 ]
+
+rest_patterns = format_suffix_patterns(rest_patterns)
--- borgia/rest/views.py
+++ borgia/rest/views.py
@@ -1,9 +1,23 @@
 from users.models import User
-from rest_framework import viewsets
 from rest.serializers import UserSerializer
+from rest_framework import permissions
+from rest_framework import generics
 
 
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all().order_by('id')
+class UserList(generics.ListCreateAPIView):
+    """
+    List all users or create a new user
+    """
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve, update or delete a user
+    """
+    permission_classes = [permissions.IsAuthenticated]
+    queryset = User.objects.all()
     serializer_class = UserSerializer
 
