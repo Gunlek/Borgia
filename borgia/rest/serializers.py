949869dd00f2c5e41d04b9f525d600bb22c87232
Fabien Aubret <fabien.aubret@gmail.com>
2019-11-30 15:41:57 +0100

    [Update] Implements ModelSerializer

--- borgia/rest/serializers.py
+++ borgia/rest/serializers.py
@@ -2,7 +2,7 @@ from users.models import User
 from rest_framework import serializers
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'last_name', 'first_name', 'email', 'surname', 'family', 'balance', 'year', 'campus', 'phone']
