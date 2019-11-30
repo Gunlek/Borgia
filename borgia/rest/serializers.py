8d025efcd9897f7c68518540729f7d822a46da8f
Fabien Aubret <fabien.aubret@gmail.com>
2019-11-30 17:54:07 +0100

    [Add] Add RESTFul API for shops

--- borgia/rest/serializers.py
+++ borgia/rest/serializers.py
@@ -13,3 +13,9 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = ['id', 'name', 'unit', 'is_manual', 'manual_price', 'correcting_factor', 'is_active', 'is_removed', 'shop_id']
+
+
+class ShopSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Shop
+        fields = ['id', 'name', 'description', 'color']
--- borgia/rest/views.py
+++ borgia/rest/views.py
@@ -2,6 +2,7 @@ from users.models import User
 from shops.models import Product, Shop
 from rest.serializers import UserSerializer
 from rest.serializers import ProductSerializer
+from rest.serializers import ShopSerializer
 from rest_framework import permissions
 from rest_framework import generics
 
@@ -34,4 +35,16 @@ class ProductDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+
+class ShopList(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Shop.objects.all()
+    serializer_class = ShopSerializer
+
+
+class ShopDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Shop.objects.all()
+    serializer_class = ShopSerializer
 
